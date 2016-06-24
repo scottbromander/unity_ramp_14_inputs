fbe0aaa7806014544fd93fd0e50267d12e422cf4
Scott Bromander <scott@primeacademy.io>
2016-06-24 11:16:32 -0500

    initial commit

---
+++ Assets/DisplayChangedTextContent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayChangedTextContent : MonoBehaviour {
+	private InputField inputField;
+
+	// Use this for initialization
+	void Start () {
+		inputField = GetComponent<InputField>();
+	}
+	
+	// Update is called once per frame
+	public void PrintNewValue () {
+		string msg = "new content = '" + inputField.text + "'";
+		print(msg);
+	}
+}
